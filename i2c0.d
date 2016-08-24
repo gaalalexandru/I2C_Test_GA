bf45fb03bc7a66c5abdee0b919269a7d846cd46d
gaalalexandru <gsx_16@yahoo.com>
2016-08-24 23:40:29 +0300

    initial_commit

---
+++ i2c0.d
@@ -0,0 +1,3 @@
+.\i2c0.o: I2C0.c
+.\i2c0.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\i2c0.o: inc/tm4c123gh6pm.h
---
+++ i2ctestmain.d
@@ -0,0 +1,6 @@
+.\i2ctestmain.o: I2CTestMain.c
+.\i2ctestmain.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\i2ctestmain.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\i2ctestmain.o: I2C0.h
+.\i2ctestmain.o: PLL.h
+.\i2ctestmain.o: UART.h
